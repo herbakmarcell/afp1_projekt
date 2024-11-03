a0e7a03c8e6110cc3f70414753349e23a63a2b08
Vastag Demeter <demetervastag05@gmail.com>
2024-11-03 20:26:17 +0100

    Órarend lekérdezésnél ellenőrzni, hogy van-e megadva ID

--- backend/routes/orarendRoutes.js
+++ backend/routes/orarendRoutes.js
@@ -6,5 +6,20 @@ const router = express.Router();
 
 
 router.get("/orarendModositas",  validateToken , orarendModositas);
-
+router.get("/orarendLekeres", validateToken, (req, res) => {
+    console.log("Órarend lekérve"); 
+    if (!req.body.id)
+    {
+        
+        res.status(404).json("Nincs id!");
+    }
+    else 
+    {
+        const id = req.body.id;
+        console.log("id: " + id);
+        res.status(200).json("id: " + id);
+        
+        
+    }
+});
 export default router
