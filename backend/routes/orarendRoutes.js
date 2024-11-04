43d37a4f516f35db8c7d500b7f6298a533a07cf5
Vastag Demeter <demetervastag05@gmail.com>
2024-11-04 09:08:00 +0100

    Kód átvitele a Controller fájlba, órarend sikeres lekérdezése

--- backend/routes/orarendRoutes.js
+++ backend/routes/orarendRoutes.js
@@ -1,25 +1,10 @@
 import express from 'express'
-import { orarendModositas } from '../controllers/orarendController.js';
+import { orarendLekeres, orarendModositas } from '../controllers/orarendController.js';
 import validateToken from '../middleware/validateTokenHandler.js';
 
 const router = express.Router();
 
 
 router.get("/orarendModositas",  validateToken , orarendModositas);
-router.get("/orarendLekeres", validateToken, (req, res) => {
-    console.log("Órarend lekérve"); 
-    if (!req.body.id)
-    {
-        
-        res.status(404).json("Nincs id!");
-    }
-    else 
-    {
-        const id = req.body.id;
-        console.log("id: " + id);
-        res.status(200).json("id: " + id);
-        
-        
-    }
-});
+router.get("/orarendLekeres", validateToken, orarendLekeres);
 export default router
