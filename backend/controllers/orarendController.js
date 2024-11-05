d33e98bd9ba29dbe0686114b7c1e0b97a8341f5a
Vastag Demeter <demetervastag05@gmail.com>
2024-11-05 08:45:40 +0100

    Adatok mentése, óra felvitele a rendszerbe
    Az órát még nem viszi fel a dátum miatt, javítva lesz

--- backend/controllers/orarendController.js
+++ backend/controllers/orarendController.js
@@ -28,7 +28,55 @@ const oraLetrehozas = (async (req, res) => {
   if (jogkor_id == 1) {
     return res.status(403).json("Tanuló nem hozhat létre új órát!");
   }
+  const body_idopont_eleje = req.body.idopont_eleje;
+  const body_idopont_vege = req.body.idopont_vege;
+  const body_cim = req.body.cim;
+  const body_helyszin = req.body.helyszin;
+  const body_felhasznalo_id = req.body.felhasznalo_id;
 
+  if(!body_idopont_eleje || !body_idopont_vege || !body_cim || !body_helyszin || !body_felhasznalo_id)
+  {
+    return res.status(406).json("Nincs minden adat megadva!");
+  }
+
+  console.log("OK");
+  console.log(body_idopont_eleje);
+  console.log(body_idopont_vege);
+  console.log(body_cim);
+  console.log(body_helyszin);
+  console.log(body_felhasznalo_id);
+
+  //A felvitt rekord id-je
+  let felvitt_ora_id = await prisma.orak.aggregate({
+    _max: {
+      ora_id: true
+    }
+  });
+ 
+
+  //Felviszem az orak táblába a rekordot
+  console.log("Óra felvitele...")
+  await prisma.orak.create({
+    data: {
+     
+      idopont_eleje: body_idopont_eleje,
+      idopont_vege: body_idopont_vege,
+      cim: body_cim,
+      helyszin: body_helyszin
+    }
+  }); 
+  console.log("Óra felvéve...")
+  
+  
+  // console.warn("Kapcsolótáblába felvitel...");
+  // //Felviszem a kapcsolótáblába az adatokat
+  // await prisma.orarend.create({
+  //   data:{
+  //     ora_id: felvitt_ora_id,
+  //     felhasznalo_id: body_felhasznalo_id
+  //   }
+  // });
+  // console.log("Kapcsolótáblába felvéve...")
   
 
 });
