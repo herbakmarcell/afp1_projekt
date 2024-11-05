fdd763f8e637ce24af80e6178636055e613d0ebd
Vastag Demeter <demetervastag05@gmail.com>
2024-11-05 16:15:10 +0100

    Óra hozzáadása  a rendszerhez megírva
    Sikeresen lehet új órát felvinni a rendszerbe.

--- backend/controllers/orarendController.js
+++ backend/controllers/orarendController.js
@@ -28,8 +28,16 @@ const oraLetrehozas = (async (req, res) => {
   if (jogkor_id == 1) {
     return res.status(403).json("Tanuló nem hozhat létre új órát!");
   }
-  const body_idopont_eleje = req.body.idopont_eleje;
-  const body_idopont_vege = req.body.idopont_vege;
+
+  //A megadott idő valamiért 1 órával kevesebb, 
+  //ezért hozzáadok 1 órát a fügvénnyel
+  Date.prototype.addHours= function(h){
+    this.setHours(this.getHours()+h);
+    return this;
+  }
+
+  const body_idopont_eleje = new Date(req.body.idopont_eleje).addHours(1);
+  const body_idopont_vege = new Date(req.body.idopont_vege).addHours(1);
   const body_cim = req.body.cim;
   const body_helyszin = req.body.helyszin;
   const body_felhasznalo_id = req.body.felhasznalo_id;
@@ -46,12 +54,7 @@ const oraLetrehozas = (async (req, res) => {
   console.log(body_helyszin);
   console.log(body_felhasznalo_id);
 
-  //A felvitt rekord id-je
-  let felvitt_ora_id = await prisma.orak.aggregate({
-    _max: {
-      ora_id: true
-    }
-  });
+  
  
 
   //Felviszem az orak táblába a rekordot
@@ -66,18 +69,26 @@ const oraLetrehozas = (async (req, res) => {
     }
   }); 
   console.log("Óra felvéve...")
+
+  //A felvitt rekord id-je
+  const felvitt_ora_max_id = await prisma.orak.aggregate({
+    _max: {
+      ora_id: true
+    }
+  });
+  const felvitt_ora_id = felvitt_ora_max_id._max.ora_id
+  console.log("felvitt óra id-je:" + felvitt_ora_id);
   
-  
-  // console.warn("Kapcsolótáblába felvitel...");
-  // //Felviszem a kapcsolótáblába az adatokat
-  // await prisma.orarend.create({
-  //   data:{
-  //     ora_id: felvitt_ora_id,
-  //     felhasznalo_id: body_felhasznalo_id
-  //   }
-  // });
-  // console.log("Kapcsolótáblába felvéve...")
-  
+  console.log("Kapcsolótáblába felvitel...");
+  //Felviszem a kapcsolótáblába az adatokat
+  await prisma.orarend.create({
+    data:{
+      ora_id: felvitt_ora_id,
+      felhasznalo_id: body_felhasznalo_id
+    }
+  });
+  console.log("Kapcsolótáblába felvéve...")
+  res.status(201).json("Az óra sikeresen felkerült a rendszerbe!");
 
 });
 export { orarendModositas, oraLetrehozas }
