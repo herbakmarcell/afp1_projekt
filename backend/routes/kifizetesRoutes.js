d8d8dcb4c454dbfd3145902f1b6f87cef29a1cf5
SzaboRichard01 <ricsi5822@gmail.com>
2024-11-18 17:56:46 +0100

    kifizetés végrehajtásának megírása

--- backend/routes/kifizetesRoutes.js
+++ backend/routes/kifizetesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { tanuloKifizetesei } from '../controllers/kifizetesController.js'
+import { tanuloKifizetesei, kifizetesVegrehajtasa } from '../controllers/kifizetesController.js'
 import validateToken from '../middleware/validateTokenHandler.js';
 
 const router = express.Router()
 
 router.get("/kifizetesek", validateToken, tanuloKifizetesei)
+router.put("/vegrehajtas", validateToken, kifizetesVegrehajtasa)
 
 export default router
