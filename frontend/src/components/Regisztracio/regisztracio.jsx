86e4833288bf88957c7010be2e9b23fced17e175
BMVHLESzklRichard <112695477+BMVHLESzklRichard@users.noreply.github.com>
2024-12-08 09:42:26 +0100

    Frontend - Hibaüzenetek kiírása

--- frontend/src/components/Regisztracio/regisztracio.jsx
+++ frontend/src/components/Regisztracio/regisztracio.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FormInput } from "../FormInputDivek/formInputDiv";
@@ -8,49 +8,81 @@ import {
   faLock,
 } from "@fortawesome/free-solid-svg-icons";
 import "../../Css/bejreg.css";
+import { Toast } from "primereact/toast";
 
 const RegisztracióFormDiv = () => {
   const navigate = useNavigate();
   const [vezeteknev, setVezeteknev] = useState("");
   const [keresztnev, setKeresztnev] = useState("");
-  // const [jogosultsag, setJogosultsag] = useState(0);
-  const [bankszamla, setBankszamla] = useState("");
   const [email, setEmail] = useState("");
-  const [jelszo, setJelszo] = useState(0);
+  const [jelszo, setJelszo] = useState([]);
 
-  // const handleChange = (e) => {
-  //   let value = e.target.value.replace(/\D/g, ""); // Csak számokat engedünk
-  //   value = value.replace(/(\d{4})(?=\d)/g, "$1-"); // Minden 4 számjegy után kötőjelet ad hozzá
-  //   setBankszamla(value);
-  // };
+  const toast = useRef(null);
+  const toastCenter = useRef(null);
+  const showSuccess = () => {
+    toast.current.show({
+      severity: "success",
+      summary: "Sikeres regisztráció",
+      detail: "Továbbítás a bejelentkezésre",
+      life: 2000,
+    });
+  };
+
+  const showError = (props) => {
+    const errorMess = props;
+    toastCenter.current.show({
+      severity: "error",
+      summary: "HIBA!",
+      detail: errorMess,
+      life: 2000,
+    });
+    setVezeteknev("");
+    setKeresztnev("");
+    setEmail("");
+    setJelszo("");
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Itt jönne a regisztrációs logika
-    // Mivel most nem releváns, közvetlenül navigálunk a bejelentkezéshez
-    const resp = await axios.post("http://localhost:5000/api/users/register", {
-      vezeteknev,
-      keresztnev,
-      // bankszamla,
-      email,
-      jelszo,
-    });
-    const data = resp.data;
-    console.log(data.sikeres);
-    if (data.sikeres === "Success") {
-      navigate("/bejelentkezes");
+    try {
+      // Itt jönne a regisztrációs logika
+      // Mivel most nem releváns, közvetlenül navigálunk a bejelentkezéshez
+      const resp = await axios.post(
+        "http://localhost:5000/api/users/register",
+        {
+          vezeteknev,
+          keresztnev,
+          // bankszamla,
+          email,
+          jelszo,
+        }
+      );
+      const data = resp.data;
+      console.log(data.sikeres);
+      if (data.sikeres === "Success") {
+        showSuccess();
+        setTimeout(() => {
+          navigate("/bejelentkezes"); // Visszairányítás a főoldalra
+        }, 2000);
+      }
+    } catch (error) {
+      showError(error.response.data);
+      console.error("Login failed", error);
     }
   };
 
   return (
     <div className="formDiv">
       <h1>Regisztráció</h1>
+      <Toast ref={toastCenter} position="center" />
       <form>
         <FormInput
           spanIcon={faSignature}
           type="text"
           name="Vezetéknév"
           placeholder="Vezetéknév"
+          value={vezeteknev}
           onChange={(e) => setVezeteknev(e.target.value)}
         />
 
@@ -59,34 +91,16 @@ const RegisztracióFormDiv = () => {
           type="text"
           name="Keresztnév"
           placeholder="Keresztnév"
+          value={keresztnev}
           onChange={(e) => setKeresztnev(e.target.value)}
         />
-
-        {/* <FormInput
-          iconSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQPNzqYRq287b9xJql_r-MmWqs6thrG9NKig&s"
-          iconAlt="permissionIcon"
-          type="text"
-          name="Jogosultság"
-          placeholder="Jogosultság"
-          onChange={(e) => setJogosultsag(e.target.value)}
-        /> */}
-        {/* 
-        <FormInput
-          iconSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ6Ba0qVTby-k31mhqoRyisNvb2ChdkcV9pA&s"
-          iconAlt="calculatorIcon"
-          type="text"
-          name="Bankszámla"
-          value={bankszamla}
-          onChange={handleChange}
-          placeholder="XXXX-XXXX-XXXX-XXXX"
-          maxLength="19"
-        /> */}
 
         <FormInput
           spanIcon={faEnvelope}
           type="email"
           name="E-mail"
           placeholder="autosiskola@gmail.com"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
 
@@ -95,9 +109,11 @@ const RegisztracióFormDiv = () => {
           type="password"
           name="Jelszó"
           placeholder="********"
+          value={jelszo}
           onChange={(e) => setJelszo(e.target.value)}
         />
         <div className="FormDivElementButton">
+          <Toast ref={toast} />
           <input type="submit" value="Bejelentkezés" onClick={handleSubmit} />
         </div>
       </form>
